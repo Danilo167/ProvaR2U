9bd366ff2db2040bb3bfdd4b000379097bbe9fea
danilo.caxias <dscr9669@gmail.com>
2025-04-29 09:56:54 -0300

    .

--- Item.java
+++
@@ -1,2 +0,0 @@
-//A interface foi necessária porque ela define um contrato comum entre os diferentes tipos de itens que podem ser embalados, independentemente de sua forma. Sim, “item.calculaQtdeEmbalagem()” faz uso do polimorfismo, pois o método chamado depende do tipo real do objeto (prato ou mala), mesmo que seja acessado pela interface.
-
